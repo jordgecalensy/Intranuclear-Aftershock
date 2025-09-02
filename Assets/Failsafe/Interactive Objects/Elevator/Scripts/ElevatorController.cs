e53053310ab2649077ead30a09b60d407a7c8648
kiko <kirill_costomarov@mail.ru>
2025-09-02 21:59:30 +0300

    Подключил лифт к мини-игре

--- Assets/Failsafe/Interactive Objects/Elevator/Scripts/ElevatorController.cs
+++ Assets/Failsafe/Interactive Objects/Elevator/Scripts/ElevatorController.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class ElevatorController : MonoBehaviour
 {
     private bool _canMove = false;
-    // [SerializeField] private GameObject _points;
+
+    [SerializeField] private bool _isPowered;
     [SerializeField] private float _speed = 2.0f;
     [SerializeField] private int _startFloor;
     [SerializeField] private Transform[] _points;
@@ -19,6 +20,8 @@ public class ElevatorController : MonoBehaviour
 
     void FixedUpdate()
     {
+        Debug.Log(_isPowered);
+        if (!_isPowered) return;
         if (Vector3.Distance(transform.position, _points[_pointIndex].position) < 0.01f)
         {
             _canMove = false;
@@ -29,9 +32,9 @@ public class ElevatorController : MonoBehaviour
             transform.position = Vector3.MoveTowards(transform.position, _points[_pointIndex].position, _speed * Time.deltaTime);
         }
     }
-
     public void OnButtonUpPress()
     {
+        if (!_isPowered) return;
         if (_canMove) return;
         if (_pointIndex == _points.Length - 1) return;
 
@@ -39,9 +42,9 @@ public class ElevatorController : MonoBehaviour
         _pointIndex++;
         _canMove = true;
     }
-
     public void OnButtonDownPress()
     {
+        if (!_isPowered) return;
         if (_canMove) return;
         if (_pointIndex == 0) return;
 
@@ -51,6 +54,7 @@ public class ElevatorController : MonoBehaviour
     }
     public void CallElevatorButton(int floorNumber)
     {
+        if (!_isPowered) return;
         if (_canMove) return;
         if (_pointIndex == floorNumber) return;
 
@@ -58,5 +62,15 @@ public class ElevatorController : MonoBehaviour
         _pointIndex = floorNumber;
         _canMove = true;
 
+    }
+    public void OnPowered()
+    {
+        Debug.Log($"{gameObject} power on");
+        _isPowered = true;
+    }
+    public void OffPowered()
+    {
+        Debug.Log($"{gameObject} power off");
+        _isPowered = false;
     }
 }
