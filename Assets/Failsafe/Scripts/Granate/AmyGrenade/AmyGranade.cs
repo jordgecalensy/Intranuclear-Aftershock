ba548e1dec1cf5bb93108e2e0e04863319a60c74
kiko <kirill_costomarov@mail.ru>
2026-02-11 23:35:05 +0300

    Удалил прошлую реализацию

--- Assets/Failsafe/Scripts/Granate/AmyGrenade/AmyGranade.cs
+++
@@ -1,8 +0,0 @@
-﻿public class AmyGranade : Granade
-{
-    protected override void ExplosionEffect()
-    {
-
-    }
-}
-
--- Assets/Failsafe/Scripts/Granate/FireGrenade/FirestormGranade.cs
+++
@@ -1,8 +0,0 @@
-﻿public class FirestormGranade : Granade
-{
-    protected override void ExplosionEffect()
-    {
-
-    }
-}
-
